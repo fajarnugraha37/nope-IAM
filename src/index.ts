ecd61c49df28472f623a150679c5475e2aca0dcf
fajar <nugrahafajar37@gmail.com>
2025-07-27 17:28:36 +0700

    fix: incomplete export components

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,26 @@
  * @packageDocumentation
  */
 
-export * from './core/iam';
-export * from './types/entities';
-export * from './types/decision';
 export * from './adapters/inMemoryAdapter';
 export * from './adapters/jsonFileAdapter';
+
+export * from './core/defaultEvaluator';
+export * from './core/evaluator';
+export * from './core/iam';
+export * from './core/logger';
+export * from './core/storage';
+
 export * from './decorators/accessControl';
+
+export * from './frameworks/eventDriven';
+export * from './frameworks/express';
+export * from './frameworks/fastify';
+export * from './frameworks/hono';
+export * from './frameworks/nestjs';
+export * from './frameworks/serverless';
+
+export * from './types/decision';
+export * from './types/entities';
+
 export * from './utils/roleAssignment';
 export * from './utils/serialization';
-export * from './core/evaluator';
-
-export * from './frameworks/express';
-export * from './frameworks/hono';
-export * from './frameworks/fastify';
-export * from './frameworks/nestjs';
-export * from './frameworks/eventDriven';
-export * from './frameworks/serverless';
